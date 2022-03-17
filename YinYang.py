0b969ff16c129713053d8841180cc86de3a02a14
Oke Amuwha <amuwhao46@gmail.com>
2022-03-17 18:09:39 -0400

    Added coordinates
    Added coordinates for where and how big the circle should be. Trying to figure out the equation

--- YinYang.py
+++ YinYang.py
@@ -8,8 +8,14 @@ By: Oke Amuwha
 3/17/22
 """
 #Coordinates
-x = 
-y =
+x = np.linspace(0, 2, 128)
+y = np.linspace(0, 2, 128)
+h = 0
+k = 0
+radius = 16
+
+#Equation
+
 
 #Plot and show data
 plt.plot(x,y)
