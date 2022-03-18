66bd65eb35f4d2b7da04f7e83f88cb7db5b0eaa0
Oke Amuwha <amuwhao46@gmail.com>
2022-03-17 20:38:40 -0400

    Plotted general concept
    Made a general concept of what I wanted to create, will fill in top and bottom layers and clean up

--- YinYang.py
+++ YinYang.py
@@ -6,6 +6,7 @@ import numpy as np
 By: Oke Amuwha
 3/17/22
 """
+
 theta = np.linspace(0, 2 * np.pi, 100)
 radius = 4
 
@@ -14,9 +15,10 @@ x = radius * np.cos(theta)
 y = radius * np.sin(theta)
 
 #coordinates for sine wave
-period = np.linspace(0, 2 * np.pi, 100)
-y2 = np.sin(period)
+xsin = np.linspace(-4, 4, 100)
+ysin = np.sin(xsin)
 
 #Plot and show data
-plt.plot(period, y2)
+plt.plot(x, y)
+plt.plot(xsin, ysin)
 plt.show()
